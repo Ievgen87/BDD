69f9d95fc7b843f5730fafdb878c81b2692437a0
Ромашко <romashko-bor@mail.ru>
2024-06-06 21:49:33 +0300

    Add test

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -3,34 +3,54 @@ package ru.netology.web.page;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
+
 public class DashboardPage {
-    private SelenideElement heading = $("[data-test-id=dashboard]");
-    private ElementsCollection topUpButtons = $$("button[data-test-id=action-deposit]");
-    public SelenideElement card1 = $("div[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
-    public SelenideElement card2 = $("div[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+    private final SelenideElement heading = $("[data-test-id=dashboard]");
+    private final ElementsCollection cards = $$(".list__item div");
+    private final SelenideElement reloadButton =$("[data-test-id='action-reload']");
 
 
-    public DashboardPage() {
+    public DashboardPage(){
+        heading.shouldBe(Condition.visible);
+    }
+
+
+    public int getCardBalance(String maskedCarNumber) {
+        var text = cards.findBy(Condition.text(maskedCarNumber)).getText();
+        return extractBalance(text);
+    }
+
+
+    public int getCardBalance(int index) {
+        var text = cards.get(index).getText();
+        return extractBalance(text);
+    }
+
+
+    public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo){
+        cards.findBy(Condition.attribute("data-test-id",cardInfo.getTestId())).$("button").click();
+        return new TransferPage();
+    }
+
+    public void reloadDashboardPage(){
+        reloadButton.click();
         heading.shouldBe(visible);
     }
 
-    public TopUpPage clickTopUp(SelenideElement card) {
-
-        card.find("button[data-test-id=action-deposit]").click();
-        return new TopUpPage();
+    private int extractBalance(String text){
+        var start = text.indexOf(balanceStart);
+        var finish = text.indexOf(balanceFinish);
+        var value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
     }
-
-    public int getBalance(SelenideElement card) {
-        String [] text = card.innerText().split(" ");
-        return Integer.parseInt(text[5]);
-    }
-
-
 
 
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,75 +1,64 @@
 package ru.netology.web.test;
 
-import com.codeborne.selenide.Condition;
-import lombok.val;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
-//import ru.netology.data.DataHelper;
-//import ru.netology.page.DashboardPage;
-//import ru.netology.page.LoginPage;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
 
 
 public class MoneyTransferTest {
-    int begBalance1;
-    int begBalance2;
-    int endBalance1;
-    int endBalance2;
-    int sum;
-    DashboardPage dashboardPage;
+   DashboardPage dashboardPage;
+   CardInfo firstCardInfo;
+   CardInfo secondCardInfo;
+   int firstCardBalance;
+   int secondCardBalance;
 
-    @BeforeEach
-    void SetUp() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        dashboardPage = verificationPage.validVerify(verificationCode);
-        begBalance1 = dashboardPage.getBalance(dashboardPage.card1);
-        begBalance2 = dashboardPage.getBalance(dashboardPage.card2);
-    }
+   @BeforeEach
+    void setup(){
+      open("http://localhost:9999");
+       var loginPage = new LoginPage();
+       var authInfo = getAuthInfo();
+       var verificationPage = loginPage.validLogin(authInfo);
+       var verificationCode = getVerificationCode();
+       dashboardPage = verificationPage.validVerify(verificationCode);
+       firstCardInfo = getFirstCardInfo();
+       secondCardInfo = getSecondCardInfo();
+       firstCardBalance = dashboardPage.getCardBalance(getMaskedNumber(firstCardInfo.getCardNumber()));
+       secondCardBalance = dashboardPage.getCardBalance(1);
+   }
 
-    @Test
-    @DisplayName("Should Transfer Money From Second To First Card")
-    void shouldTransferMoneyFromSecondToFirstCard() {
-        sum = 100;
-        val topUpPage = dashboardPage.clickTopUp(dashboardPage.card1);
-        val cardNum = DataHelper.getSecondCard().getNumber();
-        val dashboardPage2 = topUpPage.successfulTopUp(Integer.toString(sum), cardNum);
-        endBalance1 = dashboardPage2.getBalance(dashboardPage2.card1);
-        endBalance2 = dashboardPage2.getBalance(dashboardPage2.card2);
-        assertEquals(begBalance1 + sum, endBalance1);
-        assertEquals(begBalance2 - sum, endBalance2);
-    }
 
     @Test
-    @DisplayName("Should Transfer Money From First To Second Card")
-    void shouldTransferMoneyFromFirstToSecondCard() {
-        sum = 100;
-        val topUpPage = dashboardPage.clickTopUp(dashboardPage.card2);
-        val cardNum = DataHelper.getFirstCard().getNumber();
-        val dashboardPage2 = topUpPage.successfulTopUp(Integer.toString(sum), cardNum);
-        endBalance1 = dashboardPage2.getBalance(dashboardPage2.card1);
-        endBalance2 = dashboardPage2.getBalance(dashboardPage2.card2);
-        assertEquals(begBalance1 - sum, endBalance1);
-        assertEquals(begBalance2 + sum, endBalance2);
-    }
+    void transferFromFirstToSecond(){
+       var amount = generateValidAmount(firstCardBalance);
+       var expectedBalanceFirstCard = firstCardBalance - amount;
+       var expectedBalanceSecondCard = secondCardBalance + amount;
+       var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+       dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+       dashboardPage.reloadDashboardPage();
+       var actualBalanceFirstCard = dashboardPage.getCardBalance(getMaskedNumber(firstCardInfo.getCardNumber()));
+       var actualBalanceSecondCard = dashboardPage.getCardBalance(1);
+       assertAll(() -> assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard),
+               () -> assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard));
+   }
 
-    @Test
-    @DisplayName("Should Not Transfer More Than Available")
-    void shouldNotTransferMoreThanAvailable() {
-        sum = begBalance1 + 100;
-        val topUpPage = dashboardPage.clickTopUp(dashboardPage.card2);
-        val cardNum = DataHelper.getFirstCard().getNumber();
-        topUpPage.successfulTopUp(Integer.toString(sum), cardNum);
-    }
+   @Test
+   void getErrorMessageIfAmountMoreBalance(){
+       var amount = generateInvalidAmount(secondCardBalance);
+       var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+       transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+       transferPage.findErrorMessage("Выполнена попытка перевода суммы, превышающей остаток на карте списания");
+       dashboardPage.reloadDashboardPage();
+       var actualBalanceFirstCard = dashboardPage.getCardBalance(getMaskedNumber(firstCardInfo.getCardNumber()));
+       var actualBalanceSecondCard = dashboardPage.getCardBalance(getMaskedNumber(secondCardInfo.getCardNumber()));
+       assertAll(() -> assertEquals(firstCardBalance, actualBalanceFirstCard),
+               () -> assertEquals(secondCardBalance, actualBalanceSecondCard));
+   }
+
 }
